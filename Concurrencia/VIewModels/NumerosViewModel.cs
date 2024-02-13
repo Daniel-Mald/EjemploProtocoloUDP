6bb83c8e3ce07a22d5c520742a828085d0704a55
Carlos Daniel Maldonado Muñoz <201G0266@rcarbonifera.tecnm.mx>
2024-02-13 08:54:10 -0600

    Agregar archivos de proyecto.

---
+++ Concurrencia/VIewModels/NumerosViewModel.cs
@@ -0,0 +1,52 @@
+﻿using GalaSoft.MvvmLight.Command;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Concurrencia.VIewModels
+{
+    public class NumerosViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+        public NumerosViewModel()
+        {
+            SumarCommand = new RelayCommand(SumarAsync);
+        }
+
+        private void SumarSincrono()
+        {
+            long suma = 0;
+            for (long i = 0; i < 1000000000; i++)
+            {
+                suma += i;
+            }
+            Suma = suma;
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Suma)));
+        }
+        private async void SumarAsync()
+        {
+            _= Task.Run(() =>
+            {
+                long suma = 0;
+                for (long i = 0; i < 1000000000; i++)
+                {
+                    suma += i;
+                }
+                Suma = suma;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Suma)));
+            });
+            
+           
+        }
+        public ICommand SumarCommand { get; set; }  
+        public long Suma { get; private set; }
+
+
+
+
+    }
+}
---
+++ EjemploUDPservidor/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//SERVIDOR UDP
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+
+//Bind
+IPEndPoint endPoint = new(IPAddress.Any, 5001);
+UdpClient server = new(endPoint);
+server.EnableBroadcast = true;  
+while (true)
+{
+    byte[] buffer = server.Receive(ref endPoint);
+
+    string mensaje = Encoding.UTF8.GetString(buffer);
+
+    Console.WriteLine("Mensaje recibido: " + mensaje);
+
+}
+
+
